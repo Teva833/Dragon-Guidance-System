5d5af3f65ec014b6c3a080a2c3cb7c6e32ef6199
Fairman.Mitchell.s651828 <s651828@rrisd.local>
2018-04-23 08:53:05 -0500

    Initial commit

---
+++ Actor.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Actor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Actor
+{
+    public Actor()
+    {
+       
+    }
+}
