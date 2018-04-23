673fa00f2bebd5e3a507bc499f336b5a66db9565
Ozzello.Marco.s157274 <s157274@rrisd.local>
2018-04-23 10:24:13 -0500

    Solver
    Added solver class

---
+++ Solver.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Solver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Solver
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Solver
+     */
+    public Solver()
+    {
+        // initialise instance variables
+        
+    }
+
+    
+}
