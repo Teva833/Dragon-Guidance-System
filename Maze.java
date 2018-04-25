89390c67398d767b4f658f7afea8b2154d835803
Marco Ozzello <38534778+M0zzello@users.noreply.github.com>
2018-04-25 10:08:55 -0500

    Added Classes from Lab 18

---
+++ Maze.java
@@ -0,0 +1,37 @@
+import mayflower.*;
+
+/**
+ * Write a description of class Map here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Maze
+{
+    Graph<Integer> map = new AdjacencyListGraph<Integer>();
+    
+    /**
+     * Constructor for objects of class Map
+     */
+    public Maze()
+    {
+        // initialise instance variables
+        map.add(0);
+        map.add(1);
+        map.add(2);
+        map.add(3);
+        map.add(4);
+        map.add(5);
+        map.add(6);
+        //add edges
+        map.addEdge(0,1);
+        map.addEdge(0,2);
+        map.addEdge(1,3);
+        map.addEdge(2,4);
+        map.addEdge(3,6);
+        map.addEdge(4,5);
+        map.addEdge(5,3);
+    }
+
+    
+}
