550c0ba1a8401a5577b428e32af6d716e64bfdf1
Marco Ozzello <38534778+M0zzello@users.noreply.github.com>
2018-04-27 09:53:48 -0500

    Made the pathFind Method

--- Solver.java
+++ Solver.java
@@ -3,14 +3,14 @@ import java.util.LinkedList;
 import java.util.List;
 /**
  * Write a description of class Solver here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Solver
 {
     // instance variables - replace the example below with your own
-
+    List<String> traversed;
 
     /**
      * Constructor for objects of class Solver
@@ -18,19 +18,53 @@ public class Solver
     public Solver()
     {
         // initialise instance variables
-        
+        traversed = new ArrayList<>();
     }
-    
+
     public List<String> pathFind(String start, String end, AdjacencyListGraph<String> graph)
     {
         List<String> path = new ArrayList<>();
         path.add(start);
-        path.add(end);
-        return path;
-    }
-    
-    
-    
+        traversed.add(start);
+        if(start.equals(end))
+        {
+            return path;
 
-    
+        }
+        List<String> neighbors = start.getNeighbors();
+        for(int i = 0; i < neighbors.length(); i++)
+        {
+            boolean passed = false;
+            for(int j = 0; j < traversed.length(); j++)
+            {
+                if(neighbors.get(i).equals(traversed.get(j)))
+                {
+                    passed = true;
+                    continue;
+                }
+            }
+            if(passed)
+            {
+                continue;
+            }
+            start = neighbors.get(i);
+            List<String> check = pathFind(start, end, graph);
+            if(check != null)
+            {
+                for(int i = 0; i < check.length(); i++)
+                {
+                    path.add(check.get(i));
+                }
+                return path;
+            }
+        }
+        //path.add(end);
+        return null;
+    }
+
+
+
+
+
 }
+
