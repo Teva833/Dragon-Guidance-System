b8989d5e6bdc111512468686d23fabd1f5485e3e
Fairman.Mitchell.s651828 <s651828@rrisd.local>
2018-04-27 10:17:38 -0500

    added 2f nodes

--- Building.java
+++ Building.java
@@ -14,6 +14,18 @@ public class Building extends Stage
     public Building()
     {
         setBackground("600Map.png");
-        addActor(new Node(), 50, 50);
+        for(int i = 0; i < 16; i++)
+            for(int j = 0; j < 12; j++)
+                //addActor(new Node(), 50 * i, 50 * j);
+        //second floor nodes
+        addActor(new Node(), 204, 105); //2f stair top
+        addActor(new Node(), 204, 150); // 621 622 624
+        addActor(new Node(), 204, 208); //623 626
+        addActor(new Node(), 204, 286); // 625 627 628 630
+        addActor(new Node(), 204, 335); //2f stair mid
+        addActor(new Node(), 143, 335); //2f stair mid hall
+        addActor(new Node(), 204, 378); //629 631 632 634
+        addActor(new Node(), 204, 448); //Rest rooms 633
+        addActor(new Node(), 204, 490); //2f stair bot
     }
 }
