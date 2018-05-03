d7a3118c2b976ddce72dd35770f8c2c5b21573a0
Fairman.Mitchell.s651828 <s651828@rrisd.local>
2018-05-03 09:17:25 -0500

    added val

--- Node.java
+++ Node.java
@@ -2,12 +2,21 @@ import mayflower.*;
 
 public class Node extends Actor
 {
+   private String val;
    public void update()
    {
         
    }
+   public Node(String n)
+   {
+       val = n;
+       Picture node = new Picture("node.png");
+       node.resize(10,10);
+       setPicture(node);
+   }
    public Node()
    {
+       val = "";
        Picture node = new Picture("node.png");
        node.resize(10,10);
        setPicture(node);
