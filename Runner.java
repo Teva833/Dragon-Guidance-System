084ae4cd664aa8364ad858d6a2ea1b93e53c59dd
Marco Ozzello <38534778+M0zzello@users.noreply.github.com>
2018-05-11 10:20:22 -0500

    Fixed

--- Runner.java
+++ Runner.java
@@ -1,5 +1,8 @@
+
 import java.util.Scanner;
 import java.util.List;
+import java.io.*;
+import mayflower.*;
 public class Runner
 {
     // instance variables - replace the example below with your own
@@ -12,44 +15,129 @@ public class Runner
     public static void main(String[] args)
     {
         
+        
+        
+        
+        Building b = new Building();
+        Mayflower may =  new Mayflower("Dragon Guidance System", 800, 600, new Building());
+        
         boolean done = false;
         Maze m = new Maze();
         Solver s = new Solver();
+        Tree t = new Tree();
         Scanner sc = new Scanner(System.in);
         String fromNode = null;
         String toNode = null;
-
-       while(!done)
-       {
-            System.out.println("What room are you near?");
-            String from = sc.next();
-            System.out.println("What room do you want to go to?");
-            String to = sc.next();
-            Tree t = new Tree();
-
-            fromNode = s.findNode(from, t.getHalls());
-            toNode = s.findNode(to, t.getHalls());
-            if(fromNode != null && toNode != null)
+        String filename = null;
+        
+        System.out.println("Do you have a schedule file? (Y for yes)");
+        if(sc.next().equals("Y"))
+        {
+            System.out.println("Please enter the file name without the .txt");
+            filename = sc.next() + ".txt";
+            try
             {
-                done = true;
+                File file = new File(filename);
+                Scanner in = new Scanner(file);
+                while(in.hasNextLine())
+                {
+                    b = new Building();
+                    may.setStage(b);
+                    String str = in.nextLine();
+                    String [] rooms = str.split(" ");
+                    String from = rooms[0];
+                    String to = rooms[1];
+                    fromNode = s.findNode(from, t.getHalls());
+                    toNode = s.findNode(to, t.getHalls());
+                    
+                    
+                    List<String> path = s.pathFind(fromNode, toNode, m.getMap());
+                    System.out.println("Here is your path.");
+                    
+                    
+                    b.matchPath(path);
+                    while(true)
+                    {
+                        System.out.println("Are you ready for the next path? (Y for yes)");
+                        String question = sc.next();
+                        if(question.equals("Y"))
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
             }
-            else
+            catch(Exception e)
             {
-                System.out.println("Sorry, the room you entered does not exist.");
-                System.out.println("Please try again.");
-                System.out.println("");
+                e.printStackTrace();
+            }
+        }
+        
+        /*
+        try
+        {
+            File file = new File(filename);
+            Scanner in = new Scanner(file);
+            while(in.hasNextLine())
+            {
+                String str = in.nextLine();
+                String [] rooms = str.split(" ");
+                String from = rooms[0];
+                String to = rooms[1];
+                fromNode = s.findNode(from, t.getHalls());
+                toNode = s.findNode(to, t.getHalls());
+                
+                
+                List<String> path = se.pathFind(fromNode, toNode, m.getMap());
+                System.out.println("Here is your path.");
+                Building b = new Building();
+                new Mayflower("Dragon Guidance System", 800, 600, new Building());
+                b.matchPath(List<String>);
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        */
+        
+        
+        
+        
+       else
+       {
+           while(!done)
+           {
+                System.out.println("What room are you near?");
+                String from = sc.next();
+                System.out.println("What room do you want to go to?");
+                String to = sc.next();
+                
+    
+                fromNode = s.findNode(from, t.getHalls());
+                toNode = s.findNode(to, t.getHalls());
+                if(fromNode != null && toNode != null)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("Sorry, the room you entered does not exist.");
+                    System.out.println("Please try again.");
+                    System.out.println("");
+                }
+           }
+           sc.close();
+            
+            
+            List<String> path = s.pathFind(fromNode,toNode, m.getMap());
+            System.out.println("Here is your path.");
+            for(int i = 0; i < path.size(); i++)
+            {
+                System.out.println(path.get(i));
             }
        }
-       sc.close();
-        
-        
-        List<String> path = s.pathFind(fromNode,toNode, m.getMap());
-        System.out.println("Here is your path.");
-        for(int i = 0; i < path.size(); i++)
-        {
-            System.out.println(path.get(i));
-            
-        }
     
     }
 }
